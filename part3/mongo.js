6ed0cdba606da355c27feb8ce26d88547e6c67cf
Javicu07 <javicu89@gmail.com>
2023-08-24 23:27:26 +0200

    24082

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,5 +1,5 @@
 // 'npm install mongoose' makes more easy the mongodb configuration wihtout original driver
-import mongoose from 'mongoose'
+import mongoose, { Schema, model } from 'mongoose'
 import { password } from 'password'
 
 const connectionString = `mongodb+srv://javicu:${password}@cluster0.a003qqx.mongodb.net/notes?retryWrites=true&w=majority`
@@ -19,3 +19,12 @@ mongoose.connect(connectionString, {
 
 //  It´s necessary to make 'Schema' in the code, in this case when we use 'mongodb'
 //  because this database is based in 'Documents' (NoSQL)
+const noteSchema = new Schema({
+  content: String,
+  date: Date,
+  important: Boolean
+})
+
+//  With the 'Schema' created we go to make a model to build instances of the 'Schema'
+const Note = model('Note', noteSchema) // 'Note' must be in uppercase because it´s create instances of the model
+//  'Note' is singular and the collections in the base it´s in singular
