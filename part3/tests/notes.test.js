316a9c46af461b5ceb92959a221b08b1420b87a4
Javicu07 <javicu89@gmail.com>
2023-08-30 21:55:44 +0200

    30087

--- part3/tests/notes.test.js
+++ part3/tests/notes.test.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const { app, server } = require('../index')
+const Note = require('../models/Note')
+const { beforeEach } = require('node:test')
 
 const api = supertest(app)
+
+const initialNotes = [
+  {
+    content: 'Learning FullStack JS',
+    important: true,
+    date: new Date()
+  },
+  {
+    content: 'Continue learning',
+    important: true,
+    date: new Date()
+  }
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+
+  const note1 = new Note(initialNotes[0])
+  await note1.save()
+
+  const note2 = new Note(initialNotes[1])
+  await note2.save()
+})
 
 test('notes are returned as json', async () => {
   await api
@@ -13,7 +38,15 @@ test('notes are returned as json', async () => {
 
 test('there are two notes', async () => {
   const response = await api.get('/api/notes')
-  expect(response.body).toHaveLength(2)
+  expect(response.body).toHaveLength(initialNotes.length)
+})
+
+test('the first note is about FullStack', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(note => note.content)
+
+  expect(contents).toContain('Learning FullStack JS')
 })
 
 //  Hook of test, executed after finished all tests
